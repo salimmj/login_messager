cd450968f30a4798b33a52079fd02380114b2332
salim <msm2243@columbia.edu>
2017-03-19 22:46:58 -0400

    not working completely

---
+++ app.py
@@ -0,0 +1,128 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('messager.html')
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+ 
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+ 
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+ 
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
+
+
+
+
+
+
+
+
+
+"""from flask import Flask, render_template, redirect, url_for, request, Response
+from flask.ext.login import LoginManager, UserMixin, login_required
+
+
+app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin):
+    # proxy for a database of users
+    user_database = {"JohnDoe": ("JohnDoe", "John"),
+               "JaneDoe": ("JaneDoe", "Jane")}
+
+    def __init__(self, username, password):
+        self.id = username
+        self.password = password
+
+    @classmethod
+    def get(cls,id):
+        return cls.user_database.get(id)
+
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username,password = token.split(":") # naive token
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0],user_entry[1])
+            if (user.password == password):
+                return user
+    return None
+
+
+
+@app.route("/protected/",methods=["GET"])
+@login_required
+def protected():
+    return Response(response="Hello Protected World!", status=200)
+
+
+if __name__ == '__main__':
+    app.config["SECRET_KEY"] = "ITSASECRET"
+    app.run(port=5000,debug=True)
+
+
+# use decorators to link the function to a url
+@app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html')  # render a template
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('messager'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/messager', methods=['GET', 'POST'])
+@login_required
+def messager():
+    error = None
+    return render_template('messager.html', error=error)
+
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
